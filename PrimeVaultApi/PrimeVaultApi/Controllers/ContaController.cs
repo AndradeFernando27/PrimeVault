e0a161add16ad3c315f44cd2578edce1c3995201
GustavoSacoman <gustavo.sacoman23@gmail.com>
2025-05-09 22:48:46 -0300

    Created ContaController and Model Conta, Created AppDbContext, and added mysql Framework

---
+++ PrimeVaultApi/PrimeVaultApi/Controllers/ContaController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace PrimeVaultApi.Controllers;
+
+[ApiController]
+[Route("Controller")]
+public class ContaController : Controller
+{
+    private readonly AppDbContext _context;
+
+    public ContaController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public Task<IActionResult> GetUserById(int id)
+    {
+        
+    }
+}
---
+++ PrimeVaultApi/PrimeVaultApi/Db/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PrimeVaultApi.Models;
+
+namespace PrimeVaultApi.Db;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Account> Accounts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Account>()
+            .ToTable("Accounts");   
+    }
+}
